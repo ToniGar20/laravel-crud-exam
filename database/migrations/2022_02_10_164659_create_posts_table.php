946ecd1fdcc21f712bffa62652bd422572c28f51
Toni <garcia.t20@gmail.com>
2022-02-10 17:50:44 +0100

    created migration for posts table

---
+++ database/migrations/2022_02_10_164659_create_posts_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 50);
+            $table->string('heading', 50);
+            $table->integer('body');
+            $table->boolean('is_private');
+            $table->boolean('is_private');
+            $table->boolean('commentable');
+            $table->boolean('expires');
+            $table->unsignedInteger('user_id');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade'); // If a user is deleted, his contacts are deleted too
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
