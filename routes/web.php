fc74f64ad1c791e9f109ebd65190d94177785167
Toni <garcia.t20@gmail.com>
2022-02-10 17:58:47 +0100

    created controlled and added it on routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('posts', PostController::class); //->middleware(Authenticate::class, 'redirectTo');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
